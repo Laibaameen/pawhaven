49fc55e66486cde837827e7be8d4f54ffa405d7a
Laibaameen <laibaak0327@gmail.com>
2026-05-18 00:52:25 +0500

    Add speed insights

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+injectSpeedInsights();
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
