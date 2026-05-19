066b7d673869cfb6fd7b5b2fa6960911e74c0c77
Laibaameen <laibaak0327@gmail.com>
2026-05-19 10:06:46 +0500

    Fix: Gemini request format

--- api/analyze.js
+++ api/analyze.js
@@ -30,7 +30,6 @@ export default async function handler(req, res) {
                   },
                 },
                 {
-                  type: 'text',
                   text: prompt,
                 },
               ],
